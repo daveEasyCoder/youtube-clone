30cc8af62b7d118db46c87ddb57a74053804f8ed
daveEasyCoder <dawittechguru@gmail.com>
2025-02-25 22:19:16 +0300

    Initial commit

---
+++ Data/VideoData.js
@@ -0,0 +1,264 @@
+
+export let Allvideo = [{
+    id:'1',
+    tag:'indian',
+    video:'longless',
+    thumbnail:'i1.jpeg',
+    picture:'i1.jpeg',
+    videoTitle:'Pushpa 2 The Rule',
+    videoAuthor:'Marques Brownlee',
+    views:'3.4',
+    Lasts:'6 months ago'
+ },{
+    id:'2',
+    tag:'indian',
+    video:'longless',
+    thumbnail:'i2.jpeg',
+    picture:'i2.jpeg',
+    videoTitle:'Sarfira Official trailer 2024',
+    videoAuthor:'Dawit T',
+    views:'1.4',
+    Lasts:'6 months ago'
+ },{
+    id:'3',
+    tag:'indian',
+    video:'longless',
+    thumbnail:'i3.jpeg',
+    picture:'i3.jpeg',
+    videoTitle:'Vedaa Jhon Abraham Sharvari Abishek',
+    videoAuthor:'Dawit T',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'4',
+    tag:'action',
+    video:'longless',
+    thumbnail:'a1.jpg',
+    picture:'a1.jpg',
+    videoTitle:'Terminator End Of The WAR',
+    videoAuthor:'Dawit T',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'5',
+    tag:'action',
+    video:'longless',
+    thumbnail:'pic1.jpg',
+    picture:'pic1.jpg',
+    videoTitle:'Avengers Doomsday',
+    videoAuthor:'Roobert Downney',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'6',
+    tag:'action',
+    video:'longless',
+    thumbnail:'a3.jpg',
+    picture:'a3.jpg',
+    videoTitle:'007 Trailers',
+    videoAuthor:'Dawit T',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'7',
+    tag:'netflix',
+    video:'longless',
+    thumbnail:'n1.jpeg',
+    picture:'n1.jpeg',
+    videoTitle:'የነ ማማር ',
+    videoAuthor:'Bekix Man',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'8',
+    tag:'netflix',
+    video:'longless',
+    thumbnail:'n2.jpeg',
+    picture:'n2.jpeg',
+    videoTitle:'Lift_ _Official trailers Netflix',
+    videoAuthor:'Kalab zekarias',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'9',
+    tag:'netflix',
+    video:'longless',
+    thumbnail:'n3.jpg',
+    picture:'n3.jpg',
+    videoTitle:'Deadpool and Wolverine Marvel Netflix',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'10',
+    tag:'action',
+    video:'longless',
+    thumbnail:'a4.jpeg',
+    picture:'a4.jpeg',
+    videoTitle:'Agatha All Along',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'11',
+    tag:'action',
+    video:'longless',
+    thumbnail:'a5.jpeg',
+    picture:'a5.jpeg',
+    videoTitle:'Transformer One trailers',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'12',
+    tag:'action',
+    video:'longless',
+    thumbnail:'a7.jpg',
+    picture:'a7.jpg',
+    videoTitle:'Double Ismart Hindi 2025',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'13',
+    tag:'indian',
+    video:'longless',
+    thumbnail:'i4.jpeg',
+    picture:'i4.jpeg',
+    videoTitle:'Once-up-on-time',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'14',
+    tag:'indian',
+    video:'longless',
+    thumbnail:'i5.jpg',
+    picture:'i5.jpg',
+    videoTitle:'Bobby Deol Kanguva Tamil',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'15',
+    tag:'indian',
+    video:'longless',
+    thumbnail:'i6.jpg',
+    picture:'i6.jpg',
+    videoTitle:'FATEH official trailer Teasers',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'16',
+    tag:'action',
+    video:'longless',
+    thumbnail:'a1.jpg',
+    picture:'a1.jpg',
+    videoTitle:'Once-up-on-time',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'17',
+    tag:'netflix',
+    video:'longless',
+    thumbnail:'n4.jpeg',
+    picture:'n4.jpeg',
+    videoTitle:'Fortnite Netflix news',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'18',
+    tag:'netflix',
+    video:'longless',
+    thumbnail:'n5.jpg',
+    picture:'n5.jpg',
+    videoTitle:'Wednesday addam',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+   },
+  // {
+//     id:'19',
+//     tag:'netflix',
+//     video:'longless',
+//     thumbnail:'n6.jpeg',
+//     picture:'n6.jpeg',
+//     videoTitle:'Rebel ridge new netflix movie',
+//     videoAuthor:'jack john.',
+//     views:'7.4',
+//     Lasts:'6 months ago'
+//  },
+    {
+    id:'20',
+    tag:'horror',
+    video:'longless',
+    thumbnail:'h1.jpeg',
+    picture:'h1.jpeg',
+    videoTitle:'BAGMAN official trailer',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'21',
+    tag:'horror',
+    video:'longless',
+    thumbnail:'h2.jpeg',
+    picture:'h2.jpeg',
+    videoTitle:'Unfamiliar horror movie',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'22',
+    tag:'horror',
+    video:'longless',
+    thumbnail:'h3.jpeg',
+    picture:'h3.jpeg',
+    videoTitle:'The NUN 2',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'23',
+    tag:'horror',
+    video:'longless',
+    thumbnail:'h4.jpg',
+    picture:'h4.jpg',
+    videoTitle:'Evil-dead-rise',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'24',
+    tag:'horror',
+    video:'longless',
+    thumbnail:'h5.jpg',
+    picture:'h5.jpg',
+    videoTitle:'Longless new movie 2001',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'25',
+    tag:'horror',
+    video:'longless',
+    thumbnail:'h6.jpg',
+    picture:'h6.jpg',
+    videoTitle:'Tarot new movie 2001',
+    videoAuthor:'jack john.',
+    views:'7.4',
+    Lasts:'6 months ago'
+ },{
+    id:'26',
+    video:'longless',
+    thumbnail:'image43.jpeg',
+    picture:'image43.jpeg',
+    videoTitle:'Avengers',
+    videoAuthor:'Dawit T',
+    views:'7.4',
+    Lasts:'6 months ago'
+ }]
+
